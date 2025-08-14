d8684f054be73814676f9f1664debfd343752bc1
Ana Garrido Diaz <anagarri@c1r1s3.42malaga.com>
2025-08-14 13:33:37 +0200

    ADD: argument validation and threads with no race condition

--- philo.c
+++ philo.c
@@ -1,31 +1,34 @@
 #include "philo.h"
 
-void	*routine()
+void	*routine(void *arg)
 {
-	ft_putstr_fd("Thread routine done\n", 1);
+	pthread_mutex_t *mutex = (pthread_mutex_t *)arg;
+	pthread_mutex_lock(mutex);
+	ft_putstr_fd("INSIDE ROUTINE FUNCTION\n", 1);
+	pthread_mutex_unlock(mutex);
+	return NULL;
 }
 
 int	create_philosophers(t_data *data)
 {
 	int i;
 	printf("Number of philosophers: %i.\n", data->num_philosophers);
-
-	pthread_t	t[data->num_philosophers];
 	pthread_mutex_t mutex;
-
+	pthread_t	t[data->num_philosophers];
+	
 	i = 0;
 	pthread_mutex_init(&mutex, NULL);
 	while(i < data->num_philosophers)
 	{
-		//pthread_mutex_lock(&mutex);
-		if(pthread_create(&t[i], NULL, &routine, NULL) != 0)
+		if(pthread_create(&t[i], NULL, &routine, &mutex) != 0)
 		{
 			ft_putstr_fd("Error creating thread\n", 2);
 			return 1;
 		}
 		i++;
+		pthread_mutex_lock(&mutex);
 		printf("Thread %i created\n", i);
-		//pthread_mutex_unlock(&mutex);
+		pthread_mutex_unlock(&mutex);
 	}
 	i = 0;
 	while(i < data->num_philosophers)
@@ -35,9 +38,13 @@ int	create_philosophers(t_data *data)
 			ft_putstr_fd("Error finishing thread\n", 2);
 			return 1;
 		}
+		i++;
+		pthread_mutex_lock(&mutex);
 		printf("Thread %i has finish\n", i);
+		pthread_mutex_unlock(&mutex);
 	}
 	pthread_mutex_destroy(&mutex);
+	
 	printf("Done\n");
 	return (0);
 }
@@ -49,9 +56,20 @@ void	init_data(char *argv[], t_data *data, int argc)
 	data->time_to_eat = ft_atoi(argv[3]);
 	data->time_to_sleep = ft_atoi(argv[4]);
 	if (argc == 6)
-	{
 		data->num_of_time_each_must_eat = ft_atoi(argv[5]);
-	}
+	else 
+		data->num_of_time_each_must_eat = 0;
+}
+
+int	 check_valid_argument(t_data *data, int argc)
+{
+	if (data->num_philosophers < 1 || data->num_philosophers > 200)
+		return 0;
+	else if ((data->time_to_die <= 0) || (data->time_to_eat <= 0) || (data->time_to_sleep <= 0))
+		return 0;
+	else if (argc == 6 && data->num_of_time_each_must_eat <= 0)
+		return 0;
+	return 1;
 }
 
 int	main(int argc, char *argv[])
@@ -64,6 +82,11 @@ int	main(int argc, char *argv[])
 		return 1;
 	}
 	init_data(argv, &data, argc);
+	if (!check_valid_argument(&data, argc))
+	{
+		ft_putstr_fd("Error\n", 2);
+		return 1;
+	}
 	if(create_philosophers(&data) != 0)
 	{
 		return 1;
